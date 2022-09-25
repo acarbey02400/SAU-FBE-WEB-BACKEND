27cfaf60dadacb28bb3403f07335e79644911361
acarbey02400 <acarwoody01@gmail.com>
2022-09-25 14:00:00 +0300

    annoucement added

---
+++ Business/Abstract/IAnnouncementService.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IAnnouncementService
+    {
+        public IResult add(Announcement announcement);
+        public IDataResult<List<Announcement>> GetByStartDate(DateTime date);
+        public IDataResult<List<Announcement>> GetByStopDate(DateTime date);
+        public IResult update(Announcement announcement);
+        public IResult delete(Announcement announcement);
+        public IDataResult<List<Announcement>> getAll();
+        public IDataResult<Announcement> getById(int id);
+        public IDataResult<List<Announcement>> getByActiveAnnouncement();
+        public IDataResult<List<Announcement>> getByActiveInAnnouncement();
+    }
+}
---
+++ Business/Concrete/AnnouncementManager.cs
@@ -0,0 +1,70 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AnnouncementManager : IAnnouncementService
+    {
+        IAnnouncementDal _announcementDal;
+        public AnnouncementManager(IAnnouncementDal announcementDal)
+        {
+            _announcementDal = announcementDal;
+        }
+
+        public IResult add(Announcement announcement)
+        {
+            _announcementDal.Add(announcement);
+            return new SuccessResult();
+        }
+
+        public IResult delete(Announcement announcement)
+        {
+            _announcementDal.Delete(announcement);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Announcement>> getAll()
+        {
+            return new SuccessDataResult<List<Announcement>>(_announcementDal.GetAll());
+        }
+
+        public IDataResult<List<Announcement>> getByActiveAnnouncement()
+        {
+            return new SuccessDataResult<List<Announcement>>(_announcementDal.GetAll(p => p.StartDate <= DateTime.Now).Where(p=>p.status==true).ToList());
+        }
+
+        public IDataResult<List<Announcement>> getByActiveInAnnouncement()
+        {
+            return new SuccessDataResult<List<Announcement>>(_announcementDal.GetAll(p => p.StartDate >= DateTime.Now).Where(p => p.status == false).ToList());
+        }
+
+        public IDataResult<Announcement> getById(int id)
+        {
+            return new SuccessDataResult<Announcement>(_announcementDal.Get(p => p.Id == id));
+        }
+
+        public IDataResult<List<Announcement>> GetByStartDate(DateTime date)
+        {
+            return new SuccessDataResult<List<Announcement>>(_announcementDal.GetAll(p=>p.StartDate==date));
+        }
+
+        public IDataResult<List<Announcement>> GetByStopDate(DateTime date)
+        {
+            return new SuccessDataResult<List<Announcement>>(_announcementDal.GetAll(p => p.StopDate == date));
+        }
+
+        public IResult update(Announcement announcement)
+        {
+            _announcementDal.Update(announcement);
+            return new SuccessResult();
+        }
+    }
+}
